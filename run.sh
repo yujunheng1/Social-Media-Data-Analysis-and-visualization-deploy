6a5ec7bf84c7c90b9a837668a221b3294c24ae51
yujunheng1 <3500528961@qq.com>
2023-05-14 21:52:53 +1000

    initial

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-#!/usr/bin/env bash
-. ./openrc.sh; ansible-playbook -i hosts pre_install.yml
+#. ./Opensrc.sh; ansible-playbook pre_install.yml
+#. ./Opensrc.sh; ansible-playbook -i ./inventory/host.ini ./Install_depend.yml
+#. ./Opensrc.sh; ansible-playbook -i ./inventory/host.ini ./couchdb.yml
+#. ./Opensrc.sh; ansible-playbook -i ./inventory/host.ini ./dpy-mastodon.yml
+.  ./Opensrc.sh; ansible-playbook -i ./inventory/work_group.ini ./dyp-cluster.yml
