b8d500211c9b490c3f0c390c8aaa48b8511fa43f
yujunheng1 <3500528961@qq.com>
2023-05-15 15:13:20 +1000

    add role and update cluster file

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #. ./Opensrc.sh; ansible-playbook pre_install.yml
-#. ./Opensrc.sh; ansible-playbook -i ./inventory/host.ini ./Install_depend.yml
+#ansible-playbook  -i ./inventory/host.ini ./host_depend.yml
 #. ./Opensrc.sh; ansible-playbook -i ./inventory/host.ini ./couchdb.yml
 #. ./Opensrc.sh; ansible-playbook -i ./inventory/host.ini ./dpy-mastodon.yml
-.  ./Opensrc.sh; ansible-playbook -i ./inventory/work_group.ini ./dyp-cluster.yml
+ansible-playbook -vvv -i ./inventory/work_group.ini ./dyp-cluster.yml
