67589710aac4dcc9418640f94341a4fd1fdd74ce
yujunheng1 <3500528961@qq.com>
2023-05-18 18:40:49 +1000

    updata

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #. ./Opensrc.sh; ansible-playbook pre_install.yml
 #ansible-playbook  -i ./inventory/host.ini ./host_depend.yml
-#. ./Opensrc.sh; ansible-playbook -i ./inventory/host.ini ./cluster_host.yml
+#ansible-playbook -i ./inventory/host.ini ./cluster_host.yml
 #ansible-playbook -i ./inventory/host.ini ./dpy-mastodon.yml
 ansible-playbook -vvv -i ./inventory/work_group.ini ./cluster_host.yml
+#ansible-playbook -vvv -i ./inventory/host.ini ./demo1.yml
