4f76ebd0a9b4d40f2dfb4559618bb2ede4462cfe
yujunheng1 <3500528961@qq.com>
2023-05-25 23:23:23 +1000

    final ansible

--- Group32_deploy_file.sh
+++ Group32_deploy_file.sh
@@ -1,13 +1,13 @@
 #install package in localhost, create volumn,security group and instance
-#. ./Opensrc.sh; ansible-playbook --ask-become-pass create_instance.yml
+. ./Opensrc.sh; ansible-playbook --ask-become-pass create_instance.yml
 #install package in the instances
-#ansible-playbook  -i ./inventory/host.ini ./host_depend.yml
+ansible-playbook  -i ./inventory/host.ini ./host_depend.yml
 #install couchDB in a docker and config it
-#ansible-playbook -vvv -i ./inventory/host.ini ./couchDB_cluster.yml
+ansible-playbook -vvv -i ./inventory/host.ini ./couchDB_cluster.yml
 
 # deploy our mastodon application to get new data from mastodon.au server
-#ansible-playbook -i ./inventory/host.ini ./dpy-mastodon.yml
+ansible-playbook -i ./inventory/host.ini ./dpy-mastodon.yml
 # deploy our backend server and backup this server
 ansible-playbook -vvv -i ./inventory/host.ini ./dpy-backend.yml
 #deploy our front-end web page
-#ansible-playbook -vvv -i ./inventory/host.ini ./dyp-frontend.yml
+ansible-playbook -vvv -i ./inventory/host.ini ./dyp-frontend.yml
